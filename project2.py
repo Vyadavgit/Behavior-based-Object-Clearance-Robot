63d0cef72bd69dbfef392178d401612beaec3db4
Stormborn9 <amanda.whisenand33@gmail.com>
2021-11-22 23:54:41 -0600

    Commit test

---
+++ project2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+from collections import deque
+import math
+
+COLOR_BLUE = 2
+COLOR_RED = 5
+
+def follow_line()
